989e530d3d79d8be80a43511fc210d4b37153445
Josh Long <joshua.long@broadcom.com>
2026-08-29 23:13:31 +0200

    automatic save @ 2026-08-29T21:13:31.784367Z

--- service/src/main/java/com/example/service/TenantController.java
+++ service/src/main/java/com/example/service/TenantController.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import io.arconia.multitenancy.core.context.TenantContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,10 +13,13 @@ import java.util.Map;
 @ResponseBody
 class TenantController {
 
-	@GetMapping("/")
-	Map<String, String> me() {
-		return Map.of("tenant", TenantContext.getTenantIdentifier(), "user",
-				SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication().getName());
-	}
+    @GetMapping("/")
+    Map<String, String> me() {
+        var contextHolderStrategy = SecurityContextHolder.getContextHolderStrategy();
+        return Map.of(//
+                "tenant", TenantContext.getTenantIdentifier(), //
+                "user", contextHolderStrategy.getContext().getAuthentication().getName() //
+        );
+    }
 
 }
