8a57cc6d82897ead653f8e7bbff72860d4df92ee
Josh Long <joshua.long@broadcom.com>
2026-08-30 13:43:30 +0200

    automatic save @ 2026-08-30T11:43:30.745618Z

--- service/src/main/java/com/example/service/CachingDataSourceInitializer.java
+++ service/src/main/java/com/example/service/CachingDataSourceInitializer.java
@@ -6,19 +6,25 @@ import java.util.concurrent.ConcurrentHashMap;
 
 class CachingDataSourceInitializer implements DataSourceInitializer {
 
-	private final Map<String, Boolean> initialized = new ConcurrentHashMap<>();
+	private final Map<String, Boolean> initialized = //
+			new ConcurrentHashMap<>();
 
 	private final DataSourceInitializer dataSourceInitializer;
 
-	CachingDataSourceInitializer(DataSourceInitializer dataSourceInitializer) {
+	CachingDataSourceInitializer(//
+			DataSourceInitializer dataSourceInitializer) {
 		this.dataSourceInitializer = dataSourceInitializer;
 	}
 
 	@Override
-	public DataSource initialize(String tenantId, DataSource dataSource) {
-		var initialized = this.initialized.computeIfAbsent(tenantId, k -> false);
-		if (!initialized) {
-			this.dataSourceInitializer.initialize(tenantId, dataSource);
+	public DataSource initialize(String tenantId, //
+			DataSource dataSource //
+	) {
+		var initialized = this.initialized //
+			.computeIfAbsent(tenantId, _ -> false);
+		if (!initialized) {//
+			this.dataSourceInitializer //
+				.initialize(tenantId, dataSource);
 			this.initialized.put(tenantId, true);
 		}
 		return dataSource;
