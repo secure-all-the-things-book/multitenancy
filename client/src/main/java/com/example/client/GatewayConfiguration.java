f6a010d340c0bdb856884a4c2041fc989a22db1f
Josh Long <joshua.long@broadcom.com>
2026-08-31 09:39:47 +0200

    automatic save @ 2026-08-31T07:39:47.908637Z

--- client/src/main/java/com/example/client/GatewayConfiguration.java
+++ client/src/main/java/com/example/client/GatewayConfiguration.java
@@ -15,22 +15,25 @@ import static org.springframework.web.servlet.function.RouterFunctions.route;
 @Configuration
 class GatewayConfiguration {
 
-	// <.>
-	@Bean
-	@Order(Ordered.LOWEST_PRECEDENCE)
-	RouterFunction<ServerResponse> uiRoute() {
-		return route().GET("/**", http()).before(BeforeFilterFunctions.uri("http://localhost:8020")).build();
-	}
+    // <.>
+    @Bean
+    @Order(Ordered.LOWEST_PRECEDENCE)
+    RouterFunction<ServerResponse> uiRoute() {
+        return route()//
+                .GET("/**", http()).before(BeforeFilterFunctions.uri("http://localhost:8020"))//
+                .build();
+    }
 
-	// <.>
-	@Bean
-	@Order(Ordered.HIGHEST_PRECEDENCE)
-	RouterFunction<ServerResponse> apiRoute() {
-		return route().GET("/api/**", http())
-			.before(BeforeFilterFunctions.uri("http://localhost:8081"))
-			.before(BeforeFilterFunctions.rewritePath("/api", "/"))
-			.filter(TokenRelayFilterFunctions.tokenRelay())
-			.build();
-	}
+    // <.>
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    RouterFunction<ServerResponse> apiRoute() {
+        return route()
+                .GET("/api/**", http())//
+                .before(BeforeFilterFunctions.uri("http://localhost:8081"))//
+                .before(BeforeFilterFunctions.rewritePath("/api", "/"))//
+                .filter(TokenRelayFilterFunctions.tokenRelay())//
+                .build();
+    }
 
 }
